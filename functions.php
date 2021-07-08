b816b524c73518247393b9958ac4bd1bc0908eed
Code IT Hub <78258092+codeithub@users.noreply.github.com>
2021-07-08 14:20:07 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,19 @@
+add_action( 'woocommerce_before_single_product', 'codeithub_prev_next_product' );
+ 
+
+add_action( 'woocommerce_after_single_product', 'codeithub_prev_next_product' );
+ 
+function codeithub_prev_next_product(){
+ 
+echo '<div class="prev_next_buttons">';
+ 
+   // 'product_cat' will make sure to return next/prev from current category
+        $previous = next_post_link('%link', '&larr; PREVIOUS', TRUE, ' ', 'product_cat');
+   $next = previous_post_link('%link', 'NEXT &rarr;', TRUE, ' ', 'product_cat');
+ 
+   echo $previous;
+   echo $next;
+    
+echo '</div>';
+         
+}
